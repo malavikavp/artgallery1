c42dbef42ea8cc01666c8bfdb946baf2b4f7073d
malavikavp <malavikavp152@gmail.com>
2025-08-16 23:28:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const onIntersect = (entries) => {
-  entries.forEach((e) => { if (e.isIntersecting) e.target.classList.add('show'); });
-};
-const io = new IntersectionObserver(onIntersect, { threshold: 0.1 });
-document.querySelectorAll('.fade-in').forEach(el => io.observe(el));
